d7353fd895bca69d0f7362c924d8276933fbab4c
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-03-15 22:49:51 -0300

    feat: add use case to create gym

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,5 +1,6 @@
-import { Gym } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client';
 import { GymsRepository } from '../gyms-repository';
+import { randomUUID } from 'node:crypto';
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
@@ -8,6 +9,22 @@ export class InMemoryGymsRepository implements GymsRepository {
     const gym = this.items.find((item) => item.id === gym_id);
 
     if (!gym) return null;
+
+    return gym;
+  }
+
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: Prisma.Decimal(data.latitude.toString()),
+      longitude: Prisma.Decimal(data.longitude.toString()),
+      create_at: new Date(),
+    };
+
+    this.items.push(gym);
 
     return gym;
   }
