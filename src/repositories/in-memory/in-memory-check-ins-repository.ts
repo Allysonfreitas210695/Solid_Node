4f131e12cdd05be3b84158143eb2d4ac0f163b45
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-03-15 23:03:14 -0300

    feat: add use case history

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -19,6 +19,11 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkIn;
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
+  }
   async findByUserIdOnDate(
     userId: string,
     date: Date,
